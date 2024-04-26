66b871fc21d0bec4690d78781ecb5f84a395b968
cxrvus <benito.m.rabe@gmail,com>
2024-04-26 13:32:37 +0200

    added basic clap parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use clap::{arg, command, ArgAction};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = command!()
+        .version("1.0")
+        .author("Benito Rabe")
+        .about("Rust Interface for Obsidian")
+        .arg(
+            arg!("command")
+                .action(ArgAction::Set)
+                .required(true)
+                .value_parser(clap::builder::PossibleValuesParser::new(["query"]))
+        )
+        .arg(
+            arg!("id")
+                .action(ArgAction::Set)
+                .required(true)
+        )
+        .arg(
+            arg!("note")
+                .action(ArgAction::Set)
+                .required(false)
+        )
+        .get_matches();
 }
