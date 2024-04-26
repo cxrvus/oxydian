2f84568fbf262baa193f251252a40b4964360d26
cxrvus <benito.m.rabe@gmail,com>
2024-04-26 14:28:12 +0200

    linted

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,25 @@
 use clap::{arg, command, ArgAction};
 
 fn main() {
-    let matches = command!()
-        .version("1.0")
-        .author("Benito Rabe")
-        .about("Rust Interface for Obsidian")
-        .arg(
-            arg!("command")
-                .action(ArgAction::Set)
-                .required(true)
-                .value_parser(clap::builder::PossibleValuesParser::new(["query"]))
-        )
-        .arg(
-            arg!("id")
-                .action(ArgAction::Set)
-                .required(true)
-        )
-        .arg(
-            arg!("note")
-                .action(ArgAction::Set)
-                .required(false)
-        )
-        .get_matches();
+	let matches = command!()
+		.version("1.0")
+		.author("Benito Rabe")
+		.about("Rust Interface for Obsidian")
+		.arg(
+			arg!("command")
+				.action(ArgAction::Set)
+				.required(true)
+				.value_parser(clap::builder::PossibleValuesParser::new(["query"]))
+		)
+		.arg(
+			arg!("id")
+				.action(ArgAction::Set)
+				.required(true)
+		)
+		.arg(
+			arg!("note")
+				.action(ArgAction::Set)
+				.required(false)
+		)
+		.get_matches();
 }
