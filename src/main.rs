cf5a8f9557a78d4b9d00e94fe8f0a08e71bcd809
cxrvus <benito.m.rabe@gmail.com>
2024-05-05 20:48:54 +0200

    changed command structure

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,17 @@
-use clap::{Parser, ValueEnum};
+use clap::Parser;
 
 
-#[derive(Parser)]
+#[derive(Debug, Parser)]
 #[command(version, about, long_about = None)]
 struct Cli {
-	#[arg(long)]
-	json: bool,
-	#[arg(value_enum)]
-	command: Command,
-	id: String,
+	#[arg(long, short)]
+	dry_run: bool,
+	flow: String,
 	origin: Option<String>,
 }
 
-#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
-enum Command {
-	Flow,
-	Query,
-}
 
 fn main() {
 	let args = Cli::parse();
-	println!("JSON: {}\nCommand: {:?}\nID: {}\nOrigin: {}", args.json, args.command, args.id, args.origin.unwrap_or("None".to_string()));
+	println!("{:?}", args);
 }
