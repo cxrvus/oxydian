4329509d6ab758ad6af9a8b66a7245c3137dfba8
cxrvus <benito.m.rabe@gmail.com>
2024-05-20 23:53:24 +0200

    made flows inject items instead of their note objects

--- src/flow.rs
+++ src/flow.rs
@@ -1,4 +1,4 @@
-use crate::{item::Item, note::Note, util::*, vault::Vault};
+use crate::{item::Item, util::*, vault::Vault};
 
 
 pub type FlowMap = HashMap<String, Flow>;
@@ -11,27 +11,31 @@ pub struct Flow {
 
 pub enum FlowFn {
 	FreeFn (fn(&Vault) -> Result<()>),
-	NoteFn (fn(&Vault, &Note) -> Result<()>),
-	MutatingFn (fn(&Vault, &mut Note) -> Result<()>),
+	NoteFn (fn(&Vault, &Item) -> Result<()>),
+	MutatingFn (fn(&Vault, &mut Item) -> Result<()>),
 }
 
 impl FlowFn {
 	pub fn execute(&self, config: &Vault, note_path: Option<PathBuf>) -> Result<()> {
 		match (self, note_path) {
-			(Self::FreeFn(flow), None) 				=> flow(config),
-			(Self::NoteFn(flow), Some(note_path)) 	=> flow(config, &Self::get_origin_note(note_path)?),
-			(Self::MutatingFn(flow), Some(note_path)) => flow(config, &mut Self::get_origin_note(note_path)?),
 
+			(Self::NoteFn(_) | Self::MutatingFn(_), Some(note_path)) => {
+				let mut note = Item::get(note_path).map_err(|_| anyhow!("Failed to get origin note file"))?;
+				note.note().ok_or_else(|| anyhow!("Origin note is not a markdown file"))?;
+
+				match self {
+					Self::NoteFn(flow) 		=> flow(config, &note),
+					Self::MutatingFn(flow) 	=> flow(config, &mut note),
+					_ 						=> unreachable!(),
+				}
+			} 
+
+			(Self::FreeFn(flow), None) 					=> flow(config),
+
+			(Self::NoteFn(_), None) 	=> Err(anyhow!("Note flows require an origin note")),
+			(Self::MutatingFn(_), None) => Err(anyhow!("Mutating note flows require an origin note")),
 			(Self::FreeFn(_), Some(_)) 	=> Err(anyhow!("FreeFlows do not accept an origin note")),
-			(Self::NoteFn(_), None) 		=> Err(anyhow!("Note flows require an origin note")),
-			(Self::MutatingFn(_), None) 	=> Err(anyhow!("Mutating note flows require an origin note")),
 
 		}
-	}
-
-	fn get_origin_note(note_path: PathBuf) -> Result<Note> {
-		let note_item = Item::get(note_path).map_err(|_| anyhow!("Failed to get origin note file"))?;
-		let note = note_item.note().ok_or_else(|| anyhow!("Origin note is not a markdown file"))?;
-		Ok(note)
 	}
 }
