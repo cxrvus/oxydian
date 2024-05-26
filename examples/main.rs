6d60fb4119aa1f35b7c48226261d6ad3706b82a5
cxrvus <benito.m.rabe@gmail.com>
2024-05-26 21:17:44 +0200

    main example

--- examples/main.rs
+++ examples/main.rs
@@ -1,5 +1,9 @@
 use oxydian::prelude::*;
 
+#[derive(Serialize, Deserialize, Debug)]
+struct NoteBase {
+	pub of: Vec<String>,
+}
 
 fn main() {
 	if let Err(e) = execute() { eprintln!("<!>\n{}", e); }
@@ -16,13 +20,14 @@ fn execute() -> Result<()> {
 		flows: FlowController::new().register_many(vec![
 			Flow {
 				name: "test_flow",
-				func: FreeFn(|_| {
-					println!("Hello World!");
+				func: NoteFn(|_, file| {
+					println!("content:\n{}\n", file.get_note()?.get_content());
+					println!("props:\n{:?}", file.get_note()?.get_props::<NoteBase>());
 					Ok(())
 				}),
 			},
 		])?,
-	};
+	}.execute()?;
 
 	Ok(())
 }
